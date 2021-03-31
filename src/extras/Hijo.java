255816fe8a81a38c1448b51c372e28805c778046
Davis-20 <davisinn2020@gmail.com>
2021-03-30 23:26:46 -0600

    inicialize repositorio

---
+++ src/extras/Hijo.java
@@ -0,0 +1,16 @@
+package extras;
+
+import comunity.Persona;
+
+public class Hijo extends Persona{
+
+    // public Hijo(String name, String gender, int age) {
+    //     super(name, gender, age);
+    //     //TODO Auto-generated constructor stub
+    // }
+
+
+    
+    
+    //provaremos si hereda o nel jjajaaj
+}
---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+import extras.Hijo;
+
+public class Test{
+    public static void main(String[] args){ 
+        Hijo hijo1 = new Hijo();
+        System.out.println("hijo1"+ hijo1);
+    }
+}
